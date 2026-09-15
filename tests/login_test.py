dcabca6a8699ab6fb43a9cf7ac6f7f6e48ebb723
HBOV2 <hajer.boukmicha@ratp.fr>
2026-09-15 13:11:28 +0200

    Update login_test.py

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,23 +1,14 @@
 # GitHub-test
 from playwright.sync_api import sync_playwright
 
-def test_login():
-
+def test_google():
     with sync_playwright() as p:
-
         browser = p.chromium.launch()
 
         page = browser.new_page()
 
-        page.goto("https://monsite.com/login")
+        page.goto("https://www.google.com")
 
-        page.fill("#username", "test")
-        page.fill("#password", "test123")
-
-        page.click("button[type='submit']")
-
-        page.wait_for_timeout(2000)
-
-        assert "Accueil" in page.title()
+        assert "Google" in page.title()
 
         browser.close()
