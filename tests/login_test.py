f95c603874b798f1376cc950ff4b6806997157ad
HBOV2 <hajer.boukmicha@ratp.fr>
2026-09-15 13:13:09 +0200

    Update login_test.py

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,14 +1,22 @@
 # GitHub-test
 from playwright.sync_api import sync_playwright
 
-def test_google():
+def test_google_search():
+
     with sync_playwright() as p:
+
         browser = p.chromium.launch()
 
         page = browser.new_page()
 
         page.goto("https://www.google.com")
 
-        assert "Google" in page.title()
+        page.locator("textarea[name='q']").fill("Playwright")
+
+        page.locator("textarea[name='q']").press("Enter")
+
+        page.wait_for_timeout(3000)
+
+        assert "Playwright" in page.title()
 
         browser.close()
